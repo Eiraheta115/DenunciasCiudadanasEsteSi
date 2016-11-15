787afc4fd3d7606b2b5f7d3d4318a1ce39d4f1eb
yaneth94 <marvinalvarenga96@gmail.com>
2016-11-14 21:31:27 -0300

    arreglo

--- resources/views/Layouts/denunciasadmin.blade.php
+++ resources/views/Layouts/denunciasadmin.blade.php
@@ -11,103 +11,101 @@
 
     <title>Bandeja de Denuncias</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/simple-sidebar.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/Elementos/css/main.css" type="text/css">
 </head>
 
 <body>
 
-    <div id="wrapper">
+        <div id="header">
+        <div class="top">
+        <nav id="nav">
+            <ul>
 
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
+                <li>
                     <a href="#">
                         Estados de Denuncia
                     </a>
                 </li>
                 <li>
-                    <a href="#">Recibidas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Recibidas</a>
                 </li>
                 <li>
-                    <a href="#">Aceptadas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Aceptadas</a>
                 </li>
                 <li>
-                    <a href="#">En investigación</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">En investigación</a>
                 </li>
                 <li>
-                    <a href="#">Documentadas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Documentadas</a>
                 </li>
                 <li>
-                    <a href="#">Procesadas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Procesadas</a>
                 </li>
                 <li>
-                    <a href="#">Cerradas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Cerradas</a>
                 </li>
                 <li>
-                    <a href="#">Denegadas</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Denegadas</a>
                 </li>
                 <li>
-                    <a href="#">Cerrar Sesión</a>
+                    <a href="#" id="top-link" class="skel-layers-ignoreHref">Cerrar Sesión</a>
                 </li>
             </ul>
+            </nav>
+  </div>
         </div>
-        <!-- /#sidebar-wrapper -->
+        <div id="main">
 
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
+                <!-- Intro -->
+                    <section id="top" class="one dark cover">
+                        <div class="container">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <h1>Bandeja de Denuncias.</h1>
+                        <h1 id="tema">Bandeja de Denuncias.</h1>
                         <table class="table table-striped table-hover">
                             <thead>
                                 <tr>
-                                    <th>Denunciante</th>
-                                    <th>Asunto</th>
-                                    <th>Fecha y Hora</th>
+                                    <th id="tablita">Denunciante</th>
+                                    <th id="tablita">Asunto</th>
+                                    <th id="tablita">Fecha y Hora</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <tr>
+                                    <td id="tablita">Zoila</td>
+                                    <td id="tablita">Zoila</td>
+                                    <td id="tablita">Zoila</td>
+                                </tr>
+
+                                <tr>
+                                    <td id="tablita">Zoila</td>
+                                    <td id="tablita">Zoila</td>
+                                    <td id="tablita">Zoila</td>
+                                </tr>
                                 
                                 @yield('content_bandeja')
 
                             </tbody>
                         </table>
-                    </div>
                 </div>
+
+                        </div>
+                    </section>
+
+                
+
             </div>
-        </div>
-        <!-- /#page-content-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
+    <!-- Scripts -->
+            <script src="css/Elementos/js/jquery.min.js"></script>
+            <script src="css/Elementos/js/jquery.scrolly.min.js"></script>
+            <script src="css/Elementos/js/jquery.scrollzer.min.js"></script>
+            <script src="css/Elementos/js/skel.min.js"></script>
+            <script src="css/Elementos/js/util.js"></script>
+            <script src="css/Elementos/js/main.js"></script>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
+    
 
 </body>
 
